d019f694b1acf2210dd4fa0e915522f1648e2c59
Roman Martynov <mrs885@mail.ru>
2022-04-17 10:14:58 +0300

    parcel added

---
+++ src/404.js
@@ -0,0 +1,12 @@
+const data = [1, 2, 3, 4]
+
+const app = document.getElementById('app')
+
+data.forEach(item => {
+    
+    let li = document.createElement('li')
+    li.textContent = item
+    app.appendChild(li)
+    
+    }
+)
