eabf5d5a24b4c1eeaaf83c3c4fc93f2f31a08c3c
Roman Martynov <mrs885@mail.ru>
2022-05-22 21:35:47 +0300

    link bug fixed

--- src/utils/HttpTransport.ts
+++ src/utils/HttpTransport.ts
@@ -1,8 +1,8 @@
 const METHODS = {
-    GET: 'GET',
-    POST:'POST',
-    PUT: 'PUT',
-    DELETE: 'DELETE',
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
 };
 
 type Options = {
@@ -17,76 +17,64 @@ type Options = {
 * На выходе: строка. Пример: ?a=1&b=2&c=[object Object]&k=1,2,3
 */
 function queryStringify(data: any) {
-    if(!data) return '';
-    // Можно делать трансформацию GET-параметров в отдельной функции
-    let dataArr = [];
-    let result = '';
-    for(let key of Object.keys(data)){
-        if(Array.isArray(data[key]))
-            result = `${key}=${data[key].join(',')}`;
-        else if (typeof data[key] == 'object')
-            result = `${key}=[object Object]`;
-        else 
-            result = `${key}=${data[key]}`;
-        dataArr.push(result);
-    }
-    return `?${dataArr.join('&')}`;
+  if (!data) return '';
+  // Можно делать трансформацию GET-параметров в отдельной функции
+  const dataArr = [];
+  let result = '';
+  for (const key of Object.keys(data)) {
+    if (Array.isArray(data[key])) {result = `${key}=${data[key].join(',')}`;}
+    else if (typeof data[key] === 'object') {result = `${key}=[object Object]`;}
+    else
+      {result = `${key}=${data[key]}`;}
+    dataArr.push(result);
+  }
+  return `?${dataArr.join('&')}`;
 }
 
 export class HTTPTransport {
-    get = (url: string, options: Options = {}) => {
-        let data = undefined;
-        if (options.data)
-            data = queryStringify(options.data)
-        return this.request(url, {...options, method: METHODS.GET, data: data}, options.timeout);
-    };
+  get = (url: string, options: Options = {}) => {
+    let data;
+    if (options.data) {data = queryStringify(options.data)};
+    return this.request(url, { ...options, method: METHODS.GET, data }, options.timeout);
+  };
 
-    put = (url: string, options: any = {}) => {
-             
-        return this.request(url, {...options, method: METHODS.PUT}, options.timeout);
-    };
-    
-    post = (url: string, options: any = {}) => {
-             
-        return this.request(url, {...options, method: METHODS.POST}, options.timeout);
-    };
-    
-    delete = (url: string, options: any = {}) => {
-             
-        return this.request(url, {...options, method: METHODS.DELETE}, options.timeout);
-    };
+  put = (url: string, options: any = {}) => this.request(url, {...options, method: METHODS.PUT}, options.timeout);
 
-    request(url: string, options: Options, timeout: number = 5000): Promise<XMLHttpRequest>{
-        const {method, data, headers} = options;
-    
-        return new Promise((resolve, reject) => {
-            const xhr = new XMLHttpRequest();
+  post = (url: string, options: any = {}) => this.request(url, {...options, method: METHODS.POST}, options.timeout);
 
-            if (options.method !== "GET") 
-                xhr.open(options.method, url)
-            else 
-                xhr.open(options.method, url + data);
+  delete = (url: string, options: any = {}) => this.request(url, {...options, method: METHODS.DELETE}, options.timeout);
 
-            if(headers) 
-                Object.keys(headers).forEach((key) => {
-                    xhr.setRequestHeader(key, headers[key]);
-                });
-            
-            xhr.onload = function() {
-                resolve(xhr);
-            }
-      
-            xhr.onabort = reject;
-            xhr.onerror = reject;
-            xhr.ontimeout = reject;
+  request(url: string, options: Options, timeout: number = 5000): Promise<XMLHttpRequest> {
+    const { method, data, headers } = options;
 
-            xhr.timeout = timeout;
-            
-            if (method === METHODS.GET || !data) {
-                xhr.send();
-            } else {
-                xhr.send(JSON.stringify(data));
-            }
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+
+      if (options.method !== 'GET')
+        xhr.open(options.method, url);
+      else
+        xhr.open(options.method, url + data);
+
+      if (headers)
+        Object.keys(headers).forEach((key) => {
+          xhr.setRequestHeader(key, headers[key]);
         });
-    }
+
+      xhr.onload = function () {
+        resolve(xhr);
+      };
+
+      xhr.onabort = reject;
+      xhr.onerror = reject;
+      xhr.ontimeout = reject;
+
+      xhr.timeout = timeout;
+
+      if (method === METHODS.GET || !data) {
+        xhr.send();
+      } else {
+        xhr.send(JSON.stringify(data));
+      }
+    });
+  }
 }
